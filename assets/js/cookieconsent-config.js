b7cd4f76b07d7accd9b26976d5f56bf09a823a00
Bilbo The Hobbyist <74491222+BilboTheHobbyist@users.noreply.github.com>
2024-09-21 20:57:48 +0200

    feat: localize cookieconsent messages
    files
    =====
    locales
    - assets/js/cc_en.json
    - assets/js/cc_fr.json
    - assets/js/cookieconsent-config.js
    
    add an equal to the Content-type declaration
    - default.hbs
      Content-type:= application

--- assets/js/cookieconsent-config.js
+++ assets/js/cookieconsent-config.js
@@ -1,43 +1,23 @@
-// constant cs = require('./assets/built/cookieconsent.umd.js');
 import '../built/cookieconsent.umd.js';
 
-// obtain plugin
-// var CookieConsent = initCookieConsent();
-
-// run plugin with your configuration
+/**
+ * All config. options available here:
+ * https://cookieconsent.orestbida.com/reference/configuration-reference.html
+ */
 CookieConsent.run({
-    guiOptions: {
-        consentModal: {
-            layout: "box",
-            position: "bottom left",
-            equalWeightButtons: true,
-            flipButtons: false
-        },
-        preferencesModal: {
-            layout: "box",
-            position: "right",
-            equalWeightButtons: true,
-            flipButtons: false
-        }
-    },
+
     categories: {
         necessary: {
-            readOnly: true
+            enabled: true,  // this category is enabled by default
+            readOnly: true  // this category cannot be disabled
         },
         analytics: {}
     },
+
     language: {
-        default: "fr",
-        autoDetect: "browser",
+        default: 'fr',
         translations: {
-            en: async () => {
-                const res = await fetch('./cc_en.js');
-                return await res.json();
-            },
-            fr: async () => {
-                const res = await fetch('./cc_fr.js');
-                return await res.json();
-            }            
-        }
+            fr: './assets/js/cc_fr.json'
+            }
     }
-);
+});
